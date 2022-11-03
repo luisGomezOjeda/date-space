111deef3fe6abfe81ba64ae73018bc6a4eed42b2
luis gomez <luisgomezojeda110502@gmail.com>
2022-11-03 19:32:59 -0300

    correction sections

--- assets/script/index.js
+++ assets/script/index.js
@@ -12,13 +12,12 @@ d.addEventListener("DOMContentLoaded",(e)=>{
 
 d.addEventListener("click",(e)=>{
   // home //
+  if(e.target.matches(".nav-ul-li a"))e.preventDefault();
   if(e.target.textContent === "00 Home"){
-    e.preventDefault();
     getHTML(e.target.href,e.target.dataset.css,undefined);
   }
   // destination //
-  if(e.target.textContent === "01 Destination"){
-    e.preventDefault();
+  if(e.target.textContent.includes("01")){
   getHTML(e.target.href,e.target.dataset.css,undefined,{
     urlContent:"./assets/html/destination/destination--moon-img.html",
     mainContent:".ilustration-img",
@@ -32,8 +31,7 @@ d.addEventListener("click",(e)=>{
   getData(getHTML,".info-sections input + label",".info-sections input",".info-date",".ilustration-img",".info-article");
   }
   // crew //
-  if(e.target.textContent === "02 Crew"){
-    e.preventDefault();
+  if(e.target.textContent.includes("02")){
   getHTML(e.target.href,e.target.dataset.css,undefined,{
     urlContent:"./assets/html/crew/crew-Engineer-info.html",
     mainContent:".main-info",
@@ -41,11 +39,10 @@ d.addEventListener("click",(e)=>{
     urlContent:"./assets/html/crew/crew-Engineer-img.html",
     mainContent:".main-img",
   });
-  getData(getHTML,".main-sections input + label",".main-sections input",".main-info",".main-img",);
+  getData(getHTML,".main-sections input + label",".main-sections input",".main-info",".main-img");
   }
   // tecnology //
-  if(e.target.textContent === "03 Technology"){
-    e.preventDefault();
+  if(e.target.textContent.includes("03")){
     getHTML(e.target.href,e.target.dataset.css,undefined,{
       urlContent:"./assets/html/tecnology/launch_vehicle--info.html",
       mainContent:".info-date",
